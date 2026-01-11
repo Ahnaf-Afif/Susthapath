f7ac1a38f6fbf9701f2dd7d8b9485a78ba670f5e
Ahnaf-Afif <williamhens123@gmail.com>
2026-01-11 11:09:28 +0600

    commit2

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/Shusthopath-Services-Overview-Copy/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
